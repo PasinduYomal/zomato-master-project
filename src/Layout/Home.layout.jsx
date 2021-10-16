9852512265d07531adbc546a5c1df62a6f144001
PasinduYomal <pasindujayazinghe@gmail.com>
2021-10-16 22:49:10 +0530

    Zomato master project final

---
+++ src/Layout/Home.layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import FoodTab from "../Components/FoodTab";
+import Navbar from "../Components/Navbar";
+
+const HomeLayout = (props) => {
+    return (
+    <>
+    <Navbar />
+    <FoodTab />
+    <div className="container mx-auto px-4 lg:px-20">
+        {props.children}
+        </div>
+    </> 
+    );
+};
+
+export default HomeLayout;
